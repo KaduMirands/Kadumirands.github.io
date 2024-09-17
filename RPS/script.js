72b6f71ee1f568dc0f924d61c0abd1caf2fc35b4
Carlos Eduardo <84359339+KaduMirands@users.noreply.github.com>
2024-09-17 01:58:29 -0300

    Update script.js

--- RPS/script.js
+++ RPS/script.js
@@ -1,16 +1,22 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
+const userScoreDisplay = document.getElementById('user-score')
+const computerScoreDisplay = document.getElementById('computer-score')
 const possibleChoices = document.querySelectorAll('button')
 let userChoice
 let computerChoice
+let userScore = 0;
+let computerScore = 0;
 
-possibleChoices.forEach(possibleChoices => addEventListener('click', (e) =>{
-    userChoice = e.target.id
-    userChoiceDisplay.innerHTML = userChoice
-    generateComputerChoice()
-
-}))
+possibleChoices.forEach(choice => {
+    choice.addEventListener('click', (e) => {
+        userChoice = e.target.id;
+        userChoiceDisplay.innerHTML = userChoice;
+        generateComputerChoice();
+        updateScores();
+    });
+});
 
 function generateComputerChoice() {
     const randomNumber = Math.floor (Math.random() * 3)
@@ -19,7 +25,7 @@ function generateComputerChoice() {
         computerChoice = 'rock'
     }
     if (randomNumber === 1) {
-        computerChoice = 'scissor'
+        computerChoice = 'scissors'
     }
     if (randomNumber === 2) {
         computerChoice = 'paper'
@@ -27,4 +33,16 @@ function generateComputerChoice() {
 
     computerChoiceDisplay.innerHTML = computerChoice
     
+}
+
+function updateScores() {
+    if ((computerChoice === 'rock' && userChoice === 'scissors') ||
+        (computerChoice === 'scissors' && userChoice === 'paper') ||
+        (computerChoice === 'paper' && userChoice === 'rock')) {
+        computerScore++;
+        computerScoreDisplay.innerHTML = computerScore;
+    } else if (userChoice !== computerChoice) {
+        userScore++;
+        userScoreDisplay.innerHTML = userScore;
+    }
 }
